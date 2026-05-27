8071450366d6cf7eb777868370290bacc987444a
SnailDust <whidbeyisaac@frontier.com>
2026-05-27 16:11:30 -0700

    Added success message on registration.

--- server/src/server.js
+++ server/src/server.js
@@ -83,6 +83,11 @@ app.post("/api/register", async(req, res) => {
 
     await User.create({ firstName, lastName, email, password : hashedPassword })
 
+    return res.status(201).json({
+      message: "User registered successfully.",
+      user: { firstName, lastName, email},
+    });
+
   }catch(error){
     console.error("Register error:", error);
     if(error.code === 11000) {
